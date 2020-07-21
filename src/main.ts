d775a3ffeef26e3abb4f503ab3db84fd0b2ddf0a
Artem Bazhin <bazhin.artyom@yandex.ru>
2020-07-21 21:28:40 +0300

    chore: update exported typings

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { TemplateResult } from './template-result';
+import { HookedResult } from './hooks';
+
 export { createTemplateResult as html } from '../src/template-result';
 export { createSVGTemplateResult as svg } from '../src/template-result';
 export { render } from '../src/render';
@@ -5,3 +8,5 @@ export { keyed } from '../src/keyed';
 export { hooked } from './hooks';
 export { useState } from './hooks';
 export { useEffect } from './hooks';
+
+export type RenderResult = TemplateResult | HookedResult<any>;
